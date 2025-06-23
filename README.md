f44d2fe7013fea21f98696c9776ce458c8650c3b
Ferchitamai <139306435+Ferchitamai@users.noreply.github.com>
2025-06-23 15:58:09 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ita-bot-2.0
+BOT EN DESAROLLO
+
+![image](https://github.com/user-attachments/assets/a9c80d38-5766-436f-90e0-ed94dc0cbad2)
+
+
+
+by: AntonyxT
---
+++ config.js
@@ -0,0 +1,127 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['5217223931552', 'FerShop OFC',true],
+  ['5218120787500', 'Colaborador',true],
+  ['5218120787500', 'Colaborador',true],
+]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = `𝑰𝒕𝒂 𝑩𝒐𝒕🧊`
+global.author = '𝑰𝒕𝒂 𝑩𝒐𝒕🧊'
+global.stickpack = '© FerShop'
+global.stickauth = 'By FerShop'
+global.wm = '𝑰𝒕𝒂 𝑩𝒐𝒕🧊'
+global.dev = '© Powered By Ita Bot'
+global.wait = '*Aɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ, sᴏʏ ʟᴇɴᴛa... ฅ^•ﻌ•^ฅ\n\n>🧊*'
+global.botname = '𝑰𝒕𝒂 𝑩𝒐𝒕🧊'
+global.textbot = `𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝑩𝒚 𝑰𝒕𝒂 𝑩𝒐𝒕🧊`
+global.listo = '*Aqui tiene ฅ^•ﻌ•^ฅ*'
+global.namechannel = '𝑰𝒕𝒂 𝑩𝒐𝒕🧊'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
+global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = 'https://chat.whatsapp.com/EWpDcZvJ97bFhVkQRZwTs2'
+global.canal = 'https://whatsapp.com/channel/0029Vb0G59R3rZZUQJSekZ38'
+global.insta = 'https://www.instagram.com/fer_shp11?igsh=bjBoczkyMm92MnZn'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219621426837-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: '120363375378707428@newsletter',
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//Apis
+
+global.APIs = {
+amel: 'https://melcanz.com',
+bx: 'https://bx-hunter.herokuapp.com',
+nrtm: 'https://nurutomo.herokuapp.com',
+xteam: 'https://api.xteam.xyz',
+nzcha: 'http://nzcha-apii.herokuapp.com',
+bg: 'http://bochil.ddns.net',
+fdci: 'https://api.fdci.se',
+dzx: 'https://api.dhamzxploit.my.id',
+bsbt: 'https://bsbt-api-rest.herokuapp.com',
+zahir: 'https://zahirr-web.herokuapp.com',
+zeks: 'https://api.zeks.me',
+hardianto: 'https://hardianto-chan.herokuapp.com',
+pencarikode: 'https://pencarikode.xyz',
+LeysCoder: 'https://leyscoders-api.herokuapp.com',
+adiisus: 'https://adiixyzapi.herokuapp.com',
+lol: 'https://api.lolhuman.xyz',
+fgmods: 'https://api-fgmods.ddns.net',
+pencarikode: 'https://pencarikode.xyz',
+Velgrynd: 'https://velgrynd.herokuapp.com',
+rey: 'https://server-api-rey.herokuapp.com',
+hardianto: 'http://hardianto-chan.herokuapp.com',
+shadow: 'https://api.reysekha.xyz',
+apialc: 'https://api-alc.herokuapp.com',
+botstyle: 'https://botstyle-api.herokuapp.com',
+neoxr: 'https://neoxr-api.herokuapp.com',
+ana: 'https://anabotofc.herokuapp.com/',
+kanx: 'https://kannxapi.herokuapp.com/',
+dhnjing: 'https://dhnjing.xyz'
+},
+
+global.APIKeys = {
+'https://api-alc.herokuapp.com': 'ConfuMods',
+'https://api.reysekha.xyz': 'apirey',
+'https://melcanz.com': 'F3bOrWzY',
+'https://bx-hunter.herokuapp.com': 'Ikyy69',
+'https://api.xteam.xyz': '5bd33b276d41d6b4',
+'https://zahirr-web.herokuapp.com': 'zahirgans',
+'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+'https://api.zeks.me': 'apivinz',
+'https://hardianto-chan.herokuapp.com': 'hardianto',
+'https://pencarikode.xyz': 'pais',
+'https://api-fgmods.ddns.net': 'fg-dylux',
+'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
+'https://server-api-rey.herokuapp.com': 'apirey',
+'https://api.lolhuman.xyz': 'GataDiosV2',
+'https://botstyle-api.herokuapp.com': 'Eyar749L',
+'https://neoxr-api.herokuapp.com': 'yntkts',
+'https://anabotofc.herokuapp.com/': 'AnaBot'
+} 
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
